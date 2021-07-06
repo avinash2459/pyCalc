37512a7b73810676fd284b80f4d0824259bb7ce8
Avinash Chamarthy <avchamar@in.ibm.com>
2021-07-06 00:15:40 -0500

    feature: setup Calculator addtion using csv file and added CSV reader

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,10 +1,25 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import CsvReader
+
+test_data = CsvReader('/src/UnitTestAll.csv').data
+
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['avalue1'], row['avalue2']), int(row['aresult']))
+            self.assertEqual(self.calculator.result, int(row['aresult']))
 
 
 if __name__ == '__main__':
